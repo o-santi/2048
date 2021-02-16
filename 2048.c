f0031dbd1912a099d2833ec38ed6544931d3e43b
leonardo santiago <leonardors@dcc.ufrj.br>
2021-02-15 23:17:31 -0300

    Tela do UI
    agora possui uma tela para mostrar informações sobre o jogo
    mas ainda precisa de mais integrações

--- 2048.c
+++ 2048.c
@@ -10,7 +10,7 @@
 #define SQUARE_WIDTH 11
 #define SQUARE_HEIGHT 7
 #define BOARD_WIDTH 4
-#define BOARD_HEIGHT 4
+#define BOARD_HEIGHT 4 
 
 #define BACKGROUND_COLOR 100
 #define TILE_VAZIA_COLOR 101
@@ -28,7 +28,7 @@
 */
 #ifdef _WIN32
 #include <ncurses/ncurses.h>
-#elif defined(unix) || defined(__unix__) || defined(__unix)
+#elif defined(unix) || defined(__unix__) || defined(__unix) || __APPLE__
 #include <ncurses.h>
 #endif
 
@@ -95,10 +95,11 @@ void createBoard(GAME_ENV *game_environment) {
       game_environment->gameBoard[coluna*BOARD_HEIGHT + linha] = window; // Salva a window na estrutura do game env
     }
   }
+  game_environment->gameBoard[BOARD_HEIGHT*BOARD_WIDTH] = newwin(SQUARE_HEIGHT * BOARD_HEIGHT, SQUARE_WIDTH * 3, 0, BOARD_WIDTH * SQUARE_WIDTH);
 }
 
 
-void blitToScreen(GAME_ENV *game_environment) {
+void drawBoard(GAME_ENV *game_environment) {
   int index, numberAtTile, charLength, colorPairIndex;
   char *to_char_buffer = malloc(sizeof * to_char_buffer * 50); /* colocamos 50 chars so pra ter certeza, apesar de nao conhecer ninguem que chegaria em 10 digitos no 2048*/
   charLength = 0;
@@ -115,15 +116,68 @@ void blitToScreen(GAME_ENV *game_environment) {
     }
     colorPairIndex = getColorPair(numberAtTile); // pega o color index pair 
     wbkgd(game_environment->gameBoard[index], COLOR_PAIR(colorPairIndex)); // pinta o background 
+										
     wattron(game_environment->gameBoard[index], COLOR_PAIR(BACKGROUND_COLOR)); 
     box(game_environment->gameBoard[index], 0, 0);  // desenha a box default envolta da window
-    wattroff(game_environment->gameBoard[index], COLOR_PAIR(BACKGROUND_COLOR)); 
-    mvwaddstr(game_environment->gameBoard[index], SQUARE_HEIGHT/ 2, SQUARE_WIDTH / 2 - charLength ,to_char_buffer); /* print buffer to window */
+    wattroff(game_environment->gameBoard[index], COLOR_PAIR(BACKGROUND_COLOR));
+    
+    wattron(game_environment->gameBoard[index], A_BOLD); 
+    mvwaddstr(game_environment->gameBoard[index], SQUARE_HEIGHT/ 2, SQUARE_WIDTH / 2 - charLength , to_char_buffer); /* print buffer to window */
+    wattroff(game_environment->gameBoard[index], A_BOLD); 
+
     wrefresh(game_environment->gameBoard[index]); // refresh box
   }
   free(to_char_buffer);
 }
 
+void drawUiScreen(GAME_ENV *game_environment) {
+  WINDOW *ui_window;
+  char *user_actual_score = calloc(sizeof(char) * 10, sizeof(char));
+  char *user_high_score = calloc(sizeof(char) * 10, sizeof(char));
+  char *round_count = calloc(sizeof(char) * 10, sizeof(char));
+
+  char *score_string = calloc(sizeof(char) * 30, sizeof(char));
+  char *high_score_string = calloc(sizeof(char) * 30, sizeof(char));
+  char *round_string = calloc(sizeof(char) * 30, sizeof(char));
+  
+  ui_window = game_environment->gameBoard[BOARD_WIDTH * BOARD_HEIGHT];
+  wbkgd(ui_window, COLOR_PAIR(BACKGROUND_COLOR));
+  wattron(ui_window, COLOR_PAIR(BACKGROUND_COLOR));
+  box(ui_window, 0, 0);
+  wattroff(ui_window, COLOR_PAIR(BACKGROUND_COLOR));
+  mvwaddstr(ui_window, 1, 1, "2048 em C");
+  
+  strcpy(score_string, "Score: ");
+  strcpy(high_score_string, "High Score: ");
+  strcpy(round_string, "Round: ");
+  
+  sprintf(user_actual_score, "%d", game_environment->actualScore);
+  sprintf(user_high_score, "%d", game_environment->highScore);
+  sprintf(round_count, "%d", game_environment->rounds);
+
+  strcat(score_string, user_actual_score);
+  strcat(high_score_string, user_high_score);
+  strcat(round_string, round_count);
+  
+  mvwaddstr(ui_window, 2, 1, score_string);
+  mvwaddstr(ui_window, 3, 1, high_score_string);
+  mvwaddstr(ui_window, 4, 1, round_string);
+  
+  wrefresh(ui_window);
+
+  free(user_high_score);
+  free(user_actual_score);
+  free(round_count);
+  
+  free(score_string);
+  free(high_score_string);
+  free(round_string);
+}
+
+void blitToScreen(GAME_ENV *game_environment) {
+  drawBoard(game_environment);
+  drawUiScreen(game_environment);
+}
 
 int processUserMove(int userMove) {
   /* Processa o input do player e devolve a direcao escolhida baseada na letra
@@ -248,6 +302,7 @@ void executarMovimento(int direcao, GAME_ENV *game_environment) {
   for (index = 0; index < 4 - direcao; index++) {
     rotacionarMatrix90Graus(&game_environment->gamePositions);
   }
+  game_environment->rounds++;
 }
 
 
@@ -277,11 +332,20 @@ int createRandomSquare(GAME_ENV * game_environment){
   return 0;
 }
 
-void initColorPairs(void) {
-  init_pair(1, 16, 14);
-  init_pair(2, 9, 52);
-  init_pair(BACKGROUND_COLOR, 0, 7);
-  init_pair(TILE_VAZIA_COLOR, 0, 7);
+void initColorPairs(void) { /* a paleta de cores para o jogo */
+  init_pair(1, 59, 188); /* ordem: id, foreground, background */
+  init_pair(2, 59, 187); /* cores do 4 */
+  init_pair(3, 15, 180); /* cores do 8 */
+  init_pair(4, 15, 173); /* cores do 16 */
+  init_pair(5, 15, 174); /* cores do 32 */
+  init_pair(6, 15, 166); /* cores do 64 */
+  init_pair(7, 15, 186); /* cores do 128 */
+  init_pair(8, 15, 185); /* cores do 256 */
+  init_pair(9, 15, 184); /* cores do 512 */
+  init_pair(10, 15, 221); /* cores do 1024 */
+  init_pair(11, 15, 220); /* cores do 2048 */
+  init_pair(BACKGROUND_COLOR, 0, 102); 
+  init_pair(TILE_VAZIA_COLOR, 0, 102);
 }
 
 void startGameEnvironment(GAME_ENV *game_environment) {
@@ -290,15 +354,15 @@ void startGameEnvironment(GAME_ENV *game_environment) {
     start_color(); // use colors
     use_default_colors();
     initColorPairs();
-  } 
+  }
   keypad(stdscr, TRUE); // pega o input do keypad
   cbreak(); // desativa line breaking
   noecho(); // desativa printing to screen
   curs_set(0); // set cursor to be invisible
   refresh(); // refresh screen
   srand(time(NULL)); // inicializamos a seed para os numeros randomicos, baseado no tempo
-  game_environment->gamePositions = malloc(sizeof(int) * BOARD_HEIGHT * BOARD_WIDTH);
-  game_environment->gameBoard = calloc(sizeof(WINDOW) * BOARD_HEIGHT * BOARD_WIDTH, sizeof(int)); // inicializamos o array que segurará as windows
+  game_environment->gamePositions = calloc(sizeof(int) * BOARD_HEIGHT * BOARD_WIDTH, sizeof(int));
+  game_environment->gameBoard = malloc(sizeof(void*) * (BOARD_HEIGHT * BOARD_WIDTH + 1)); // inicializamos o array que segurará as windows
   game_environment->gameStatus = 1; // gamestatus será valido
   game_environment->actualScore = 0; // score inicia com 0
   game_environment->rounds = 0;
